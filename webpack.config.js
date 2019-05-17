9f9b87ebfe56a520b97862d4a391efc4c2b16351
Mr.Andrew <yrfandrew@gmail.com>
2019-05-17 15:26:12 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js',
-    library: 'Underline',
+    library: 'Subscript',
     libraryTarget: 'umd'
   }
 };
